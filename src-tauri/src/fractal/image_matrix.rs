2468904219ebdeeb88c65aecf8517b7e05183210
Derek Wiers <dtwiers08@gmail.com>
2023-10-16 22:07:45 -0500

    fix image_matrix

--- src-tauri/src/fractal/image_matrix.rs
+++ src-tauri/src/fractal/image_matrix.rs
@@ -1,25 +1,23 @@
-use num::Integer;
 
 
-pub struct ImageMatrixPixel<Scalar: Integer> {
-    r: Scalar,
-    g: Scalar,
-    b: Scalar,
-    point_count: Scalar
+pub struct ImageMatrixPixel {
+    r: u64,
+    g: u64,
+    b: u64,
+    point_count: u64
 }
 
-impl<Scalar: Integer> ImageMatrixPixel<Scalar> {
-    pub fn new(r: Scalar, g: Scalar, b: Scalar, point_count: Scalar) -> Self {
+impl ImageMatrixPixel {
+    pub fn new(r: u64, g: u64, b: u64, point_count: u64) -> Self {
         Self { r, g, b, point_count }
     }
 
-    pub fn add_point(&self, r: Scalar, g: Scalar, b: Scalar) -> Self {
-        let me = self.clone();
+    pub fn add_point(&self, r: u64, g: u64, b: u64) -> Self {
         Self::new(
-            me.r + r,
-            me.g + g,
-            me.b + b,
-            self.point_count + Scalar::one()
+            self.r + r,
+            self.g + g,
+            self.b + b,
+            self.point_count + 1
         )
     }
 }
